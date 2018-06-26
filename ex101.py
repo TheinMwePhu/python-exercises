0994faec08cc532512c0062a7d9f4aad0fc254e2
Thein Mwe Phu <theinmwephu97@gmail.com>
2018-06-26 12:45:10 +0630

    change cat

--- ex101.py
+++ ex101.py
@@ -1,16 +1,16 @@
-tabby_dog="\tI'm tabble in."
-persian_dog="I'm aplit\non a line."
-backslash_dog="I'm\\a\\dog."
+tabby_cat="\tI'm tabble in."
+persian_cat="I'm aplit\non a line."
+backslash_cat="I'm\\a\\dog."
 
-fat_dog="""
+fat_cat="""
 I'll do a list:
         \t" Cat food
         \t" fishies
         \t" Catnip\n\t" Grass
         """
 
-print(tabby-dog)
-print(persian_dog)
-print(backslash_dog)
-print(fat_dog)
+print(tabby-cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
 
